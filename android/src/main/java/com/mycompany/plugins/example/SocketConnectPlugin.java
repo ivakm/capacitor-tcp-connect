c74a5959e9d33c7a45ecf93b112093c060bf3c13
Dmytro Shevchenko <dmytro.shevchenko@e-hps.com>
2023-08-09 14:50:45 -0400

    updated java

--- android/src/main/java/com/mycompany/plugins/example/SocketConnectPlugin.java
+++ android/src/main/java/com/mycompany/plugins/example/SocketConnectPlugin.java
@@ -4,7 +4,7 @@ import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.annotation.CapacitorPlugin;
-import com.getcapacitor.annotation.PluginMethod;
+import com.getcapacitor.PluginMethod;
 
 import java.io.OutputStream;
 import java.net.Socket;
